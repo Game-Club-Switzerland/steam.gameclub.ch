1ede53267f2381ba5acd71797b3f823985209e3d
d3lta3w <d3lta@game-club.ch>
2025-10-07 16:01:43 +0200

    Rename HTML table ID in Markdown file generation for clarity

--- src/command/steamGroupPlayerPlaytime.py
+++ src/command/steamGroupPlayerPlaytime.py
@@ -70,8 +70,7 @@ def createMarkdownFile(groupID64):
         f.write("""<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css"/>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-
-<table id="steam-members" class="display" style="width:100%">
+<table id="charts-table" class="display" style="width:100%">
     <thead>
         <tr>
             <th>Player</th>
